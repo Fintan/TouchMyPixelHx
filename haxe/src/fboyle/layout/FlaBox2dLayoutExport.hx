242d9c344c6a1f0c46f7b83a57582e5e44a31a1c
Fintan <fintanboyle@gmail.com>
2011-05-09 22:46:23 +0100

    small bug fix

--- haxe/src/fboyle/layout/FlaBox2dLayoutExport.hx
+++ haxe/src/fboyle/layout/FlaBox2dLayoutExport.hx
@@ -1,34 +1,8 @@
-/**
- * 
- * FlaLayoutExport by Fintan Boyle
- * Visit www.fboyle.com
- * 
- * Copyright (c) 2010 Fintan Boyle 
- * 
- * 
- * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
- * 
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * 
- */
-
+//
+//  FlaLayoutExport
+//
+//  Created by Fintan Boyle on 2011-01-25.
+//  Copyright (c) 2011 Modello Design Ltd. All rights reserved.
 //
 //	Based on Touchmypixel library
 //
@@ -49,6 +23,7 @@ import jsfl.SymbolItem;
 import jsfl.Timeline;
 
 import touchmypixel.geom.Triangulator;
+//import touchmypixel.game.geom.Triangulator;
 
 import touchmypixel.game.utils.JSFLTools;
 
@@ -266,7 +241,9 @@ class FlaBox2dLayoutExport {
 				case "frameHeight":  frameHeight = def.get(v);
 				case "sheetindicies":
 					var arr:Array<Dynamic> = cast (def.get(v), Array<Dynamic>);
-				  	sheetindicies = [arr[0].value,arr[1].value];
+					for(i in 0...arr.length){
+						sheetindicies[i] = arr[i].value;
+					}
 			}
 		}
 		
